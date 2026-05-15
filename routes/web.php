f10048da537447aec6a0d31d5f97571b69f7cdbd
Vikashh08 <vikashroy2333@gmail.com>
2026-05-15 13:18:59 +0530

    fixed TicketController import in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\AdminLoginController;
 use App\Http\Controllers\Auth\AdminRegisterController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 use App\Models\Event;
